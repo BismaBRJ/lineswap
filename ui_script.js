b5ca82fadbda4298a8871223bee14107ccd13f29
BismaBRJ <bismabrj@gmail.com>
2026-04-03 09:52:14 +0700

    Switching from snake_case to camelCase since apparently that is JavaScript naming convention carying over from Java, meanwhile I was too used to Python conventions lol

--- ui_script.js
+++ ui_script.js
@@ -1,83 +1,83 @@
 // Copyright (c) 2026 Bisma Rohpanca Joyosumarto - BismaBRJ (https://www.github.com/BismaBRJ/)
 
 // upon first loading the page (not subsequent "reloads")
-let input_lines = ["", ""];
-render_lines();
+let inputLines = ["", ""];
+renderLines();
 
-function render_lines() {
-    const n = input_lines.length;
-    const input_lines_div = document.getElementById("input_lines_div");
-    input_lines_div.innerHTML = "";
+function renderLines() {
+    const n = inputLines.length;
+    const inputLinesDiv = document.getElementById("inputLinesDiv");
+    inputLinesDiv.innerHTML = "";
 
     for (let idx = 0; idx < n; idx++) {
         if (n > 2) {
             // create delete button
-            let delete_button = document.createElement("button");
-            delete_button.id = `delete_button_${idx+1}`;
-            delete_button.innerHTML = "Delete";
-            delete_button.addEventListener("click", () => delete_input_box(idx));
-            input_lines_div.appendChild(delete_button);
+            let deleteButton = document.createElement("button");
+            deleteButton.id = `deleteButton${idx+1}`;
+            deleteButton.innerHTML = "Delete";
+            deleteButton.addEventListener("click", () => deleteInputBox(idx));
+            inputLinesDiv.appendChild(deleteButton);
         }
 
         // create clear button
-        let clear_button = document.createElement("button");
-        clear_button.id = `clear_button_${idx+1}`;
-        clear_button.innerHTML = "Clear";
-        clear_button.addEventListener("click", () => clear_input_box(idx));
-        input_lines_div.appendChild(clear_button);
+        let clearButton = document.createElement("button");
+        clearButton.id = `clearButton${idx+1}`;
+        clearButton.innerHTML = "Clear";
+        clearButton.addEventListener("click", () => clearInputBox(idx));
+        inputLinesDiv.appendChild(clearButton);
 
         // create text box
-        let line = input_lines[idx];
+        let line = inputLines[idx];
         let box = document.createElement("textarea");
-        box.id = `input_box_${idx+1}`;
+        box.id = `inputBox${idx+1}`;
         box.rows = "4";
         box.cols = "80";
         box.value = line;
-        input_lines_div.appendChild(box);
-        input_lines_div.appendChild(document.createElement("br"));
+        inputLinesDiv.appendChild(box);
+        inputLinesDiv.appendChild(document.createElement("br"));
     }
 
-    const add_box_button = document.createElement("button");
-    add_box_button.id = "btn_add_input_box";
-    add_box_button.innerHTML = "Add input box";
-    add_box_button.addEventListener("click", add_input_box);
-    input_lines_div.appendChild(add_box_button);
-    input_lines_div.appendChild(document.createElement("br"));
+    const addBoxButton = document.createElement("button");
+    addBoxButton.id = "buttonAddInputBox";
+    addBoxButton.innerHTML = "Add input box";
+    addBoxButton.addEventListener("click", addInputBox);
+    inputLinesDiv.appendChild(addBoxButton);
+    inputLinesDiv.appendChild(document.createElement("br"));
 }
 
-function save_lines() {
+function saveLines() {
     // reset global variable
-    input_lines = [];
+    inputLines = [];
 
     let idx = 0;
     let done = false;
     while (!done) {
-        let box = document.getElementById(`input_box_${idx+1}`);
+        let box = document.getElementById(`inputBox${idx+1}`);
         if (box === null) {
             done = true;
         } else {
             let line = box.value;
-            input_lines.push(line);
+            inputLines.push(line);
             idx++;
         }
     }
 }
 
-function add_input_box() {
-    save_lines();
-    input_lines.push("");
-    render_lines();
+function addInputBox() {
+    saveLines();
+    inputLines.push("");
+    renderLines();
 }
 
-function clear_input_box(idx) {
-    save_lines();
-    input_lines[idx] = "";
-    render_lines();
+function clearInputBox(idx) {
+    saveLines();
+    inputLines[idx] = "";
+    renderLines();
 }
 
-function delete_input_box(idx) {
-    save_lines();
-    input_lines.splice(idx, 1);
-    render_lines();
+function deleteInputBox(idx) {
+    saveLines();
+    inputLines.splice(idx, 1);
+    renderLines();
 }
 
